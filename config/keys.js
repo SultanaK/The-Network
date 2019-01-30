52dd45958cc1492fcc54f4f74635273bbd906717
Manuel <manuelpadilla83@hotmail.com>
2019-01-30 02:09:33 -0600

    added JWT to user.js for login, passed in postman

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://manuel:manuel1@ds147233.mlab.com:47233/the_network"
+  mongoURI: "mongodb://manuel:manuel1@ds147233.mlab.com:47233/the_network",
+  secretOrKey: "secret"
 };
