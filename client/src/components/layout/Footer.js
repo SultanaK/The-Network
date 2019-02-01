aea0a7bd4d0ae1c55860e6c61db32d8ed14cedec
Sultana <kr.shampa@yahoo.com>
2019-01-31 19:34:52 -0600

    adding Fronted

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <footer className="bg-light text-black mt-5 p-4 text-center" >
+    <p>Copyright &copy; { new Date().getFullYear() } The Network</p>
+  </footer>
+    
+  )
+}
